a4dda70bd5188eda9de6b7e5523f1acee790f1d3
AlexNgZhuYee <52555255+zhuyeeng@users.noreply.github.com>
2023-05-25 23:45:10 +0800

    code mirror problem

--- frontend/public/pages/student/chat.php
+++ frontend/public/pages/student/chat.php
@@ -120,7 +120,7 @@
                       $friend_id_result = mysqli_fetch_array($get_friend_id_sql);
                       
                       $chats = mysqli_query($con, 
-                        "SELECT chat.chat_content, chat.chat_datetime, user.user_image, user.username FROM chat 
+                        "SELECT chat.chat_content, chat.chat_datetime, user.user_image, user.username, chat.sender_id FROM chat 
                         INNER JOIN friend_list ON friend_list.friend_list_id = chat.friend_list_id
                         INNER JOIN user ON user.user_id = chat.sender_id
                         WHERE chat.friend_list_id = ".$friend_id_result['friend_list_id']);
@@ -131,17 +131,20 @@
                         {
                       ?>
                         <div class="message_container w-full h-auto flex flex-col md:flex-row lg:flex-row justify-between p-1 lg:w-4/52 overflow-y-auto">
-                          <div class="flex flex-row gap-1">
+                          <a href="../shared/user_profile.php?id=<?php echo $chat_data['sender_id']; ?>" class="flex flex-row gap-1">
                             <img src="<?=$chat_data['user_image']?>" class="profile_img w-11 h-11 justify-start rounded-full lg:w-14 lg:h-14 md:w-14 md:h-14">  
-                              <div class="chat_user_detail">
-                                <div class="chat_user">
-                                  <a href="<?php ?>"><p class="UserName self-center"><?=$chat_data['username']?></p></a>
-                                  <p class="time_text"><?=$chat_data['chat_datetime']?></p>
-                                </div>
-                                <p class="message_text"><?=$chat_data['chat_content']?></p>
+                            <div class="chat_user_detail">
+                              <div class="chat_user">
+                                <P><?= $chat_data['sender_id']?></p>
+                                <a>
+                                  <p class="UserName self-center"><?=$chat_data['username']?></p>
+                                </a>
+                                <p class="time_text"><?=$chat_data['chat_datetime']?></p>
                               </div>
+                              <p class="message_text"><?=$chat_data['chat_content']?></p>
                             </div>
-                          </div>
+                          </a>
+                        </div>
                       <?php
                         }
                       }
@@ -163,8 +166,8 @@
 
                     $friend_id_result = mysqli_fetch_array($get_friend_id_sql);
                 ?>
-                  <input type="text" id="message" class="outline-none p-2 bg-transparent border-none w-7/12" placeholder="Type Your Message Here....." id="chat_message">
-                  <span class="chatSendBtn" id="send">Send</span>
+                  <input type="text" id="message" class="outline-none p-2 bg-transparent border-none w-7/12" placeholder="Type Your Message Here....." id="chat_message" onkeypress="handleKeyDown(event)">
+                  <button class="chatSendBtn" id="send">Send</button>
                   <?php
                   } else {
                     ?>
@@ -173,61 +176,70 @@
                   }
                   ?>
                 <!--end of message type box-->
-              </div>
+            </div>
           </div>
         </div>
       </div><!--end of content box-->
     </div>
   </div>
   <script>
-// when click on the send button
-$(document).ready(function() {
-  $("#send").on("click", function() {
-    // assign value
-    var friend_list = "<?php echo $friend_id_result['friend_list_id']; ?>";
-    var message = $("#message").val();
-
-    // pass to insert message
-    $.ajax({
-      url: "../../../../backend/insertMessage.php",
-      method: "POST",
-      data: {
-        sender_id: "<?php echo $_SESSION['user_id']; ?>",
-        friend_list: friend_list,
-        message: message
-      },
-      dataType: "text",
-      success: function(data) {
-        $("#message").val("");
-      }
-    });
-  });
+  function handleKeyDown(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("send").click();
+    }
+  }
+    
   function scrollToBottom() {
-  var chatContainer = document.getElementById("chatContainer");
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-}
-  //refresh the page
-  setInterval(function() {
-    var friend_list = "<?php echo $friend_id_result['friend_list_id']; ?>";
-    var message = $("#message").val();
+    var chatContainer = document.getElementById("chatContainer");
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+  }
 
-    $.ajax({
-      url: "../../../../backend/realTimeChat.php",
-      method: "POST",
-      data :{
-        sender_id: "<?php echo $_SESSION['user_id']; ?>",
-        friend_list: friend_list,
-      },
-      dataType:"Text",
-      success:function(data){
-        $("#chat-content").html(data);
-        scrollToBottom();
-      }
-    })
-  }, 700);
-});
+  // when click on the send button
+  $(document).ready(function() {
+    scrollToBottom();
+    $("#send").on("click", function() {
+      // assign value
+      var friend_list = "<?php echo $friend_id_result['friend_list_id']; ?>";
+      var message = $("#message").val();
 
+      // pass to insert message
+      $.ajax({
+        url: "../../../../backend/insertMessage.php",
+        method: "POST",
+        data: {
+          sender_id: "<?php echo $_SESSION['user_id']; ?>",
+          friend_list: friend_list,
+          message: message
+        },
+        dataType: "text",
+        success: function(data) {
+          $("#message").val("");
+          scrollToBottom();
+        }
+      });
+    });
 
+    //refresh the page
+    setInterval(function() {
+      var friend_list = "<?php echo $friend_id_result['friend_list_id']; ?>";
+      var message = $("#message").val();
+
+      $.ajax({
+        url: "../../../../backend/realTimeChat.php",
+        method: "POST",
+        data :{
+          sender_id: "<?php echo $_SESSION['user_id']; ?>",
+          friend_list: friend_list,
+        },
+        dataType:"Text",
+        success:function(data){
+          $("#chat-content").html(data);
+          scrollToBottom();
+        }
+      })
+    }, 700);
+  });
 </script>
   <script src="./JavaScript/hamburger.js"></script>
   <script src="./JavaScript/nav_bar.js"></script>
