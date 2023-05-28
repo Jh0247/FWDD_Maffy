5d2b190d2c83cb03ff7cdcf05b70fac0bdaba4c2
zhuyeeng <zhuyeeng0524@gmail.com>
2023-05-28 15:57:40 +0800

    delete unuseful file and homepage design

--- frontend/public/pages/student/JavaScript/hamburger.js
+++
@@ -1,7 +0,0 @@
-const menu_btn = document.querySelector('.hamburger');
-const side_nav = document.querySelector('.sidebar');
-
-menu_btn.addEventListener('click',function(){
-  menu_btn.classList.toggle('is-active');
-  side_nav.classList.toggle('active');
-});
